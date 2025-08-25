cbc362c0ba907a8d4e108ac01bf286fb15578301
Skylar Ciccolini <sjcf9r@umsystem.edu>
2025-08-25 14:16:59 -0500

    First day of class

---
+++ Ciccolini_Week1_.R
@@ -0,0 +1,78 @@
+#log function
+log(987) ##note this is natural log
+
+#check the version
+R.version
+
+#update new R version
+install.packages("installr")
+
+library(installr)
+
+check.for.updates.R()
+
+#update R
+updateR()
+
+#install and load another package
+install.packages("yarrr")
+
+library("yarrr")
+
+#create a pirate plot
+pirateplot(formula = weight ~ Time, 
+           data = ChickWeight, 
+           pal = "xmen")
+
+#help for specific function
+?pirateplot
+
+#help for a specific package
+??yarrr
+
+
+#back to log
+log(987, base = 10)
+
+#OR
+log(987, 10)
+
+
+#objects
+a <- 4.2
+
+cat <- "dog"
+
+cat == "dog"
+
+#sequences
+IDs <- 1:50 ##good for whole numbers
+
+IDs
+
+IDs <- seq(from = 1, to = 50, by = 1)
+
+IDs <- seq(from = 1, to = 50, by = 1.37)
+
+#replicates of an object
+Obs <- rep(IDs, each = 3) ##each gives replicates for each element of ID value
+
+Obs <- rep(IDs, time = 3) ##time gives replicate of entire ID data string
+
+#reps with character strings
+pets <- c("kids", "cats", "dogs")
+
+repeated_pets <- rep(pets, times = 5)
+
+
+
+
+
+
+
+
+
+
+
+
+
